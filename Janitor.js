ef118c49a4606c7430f33761f3c6121a7b1d68b7
GEP-PC\GEP <eplum@ibetinc.com>
2019-09-17 21:21:13 -0400

    first commit

---
+++ Janitor.js
@@ -0,0 +1,44 @@
+class Janitor {
+
+    constructor() {
+
+        this.namename = "Noname";      
+        this.idNumber = 0;       
+        this.sweeping = false;
+
+    }
+
+
+    isSweeping() {
+
+        return this.sweeping
+    }
+
+    startSweeping() {
+
+        this.sweeping = true;
+    }
+
+    stopSweeping() {
+
+        this.sweeping = false;
+    }
+
+
+}
+
+class VampireJanitor extends Janitor {
+
+constructor(){
+
+    super();
+
+}
+
+    drawBlood(){
+
+
+
+    }
+
+}
