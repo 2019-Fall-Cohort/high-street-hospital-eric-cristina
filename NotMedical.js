0c29cf5164657482c1c80575356edebbab700098
crismagu <cmg1205@gmail.com>
2019-09-22 16:42:24 -0400

    testing of not medical  class, working on tests for med class

--- NotMedical.js
+++ NotMedical.js
@@ -1,12 +1,12 @@
 class NotMedical extends Employee {
 constructor(_name, _id, _salary) {
-    super(_name, _id, _salary)
-
+    // super(_name, _id, _salary)
+    if(this.constructor === NotMedical){
+        throw new Error("nothing");
+    }
+       
+    // super(_name, _id, _salary) {
+    //     throw new Error("nothing");
 }
-
-
-
-
-
-
 }
+module.exports = NotMedical;
