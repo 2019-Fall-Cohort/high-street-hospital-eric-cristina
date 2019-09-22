d55146d32aa9d7c9501866ce57eae1c589e93515
crismagu <cmg1205@gmail.com>
2019-09-22 13:28:51 -0400

    added medical, not medical and employee classes

---
+++ Employee.js
@@ -0,0 +1,7 @@
+class Employee {
+    constructor(_name, _id, _salary) {
+        this.name = _name;
+        this.id = -1;
+        this.salary = -1;
+    }
+}
---
+++ Medical.js
@@ -0,0 +1,11 @@
+class Medical extends Employee {
+constructor (_name, _id, _salary,) {
+    super(_name, _id, _salary)
+    this.drawBlood = true;
+    this.careForPatient = true;
+} 
+
+
+
+
+}
